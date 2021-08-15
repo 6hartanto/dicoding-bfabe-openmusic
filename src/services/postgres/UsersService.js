11a92b848dc7d62882412053e3e667b902177ce4
Hartanto <6hartanto@gmail.com>
2021-08-15 22:04:05 +0700

    hotfix implement user registration

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -34,7 +34,7 @@ class UsersService {
 
     const result = await this._pool.query(query);
 
-    if (result.rows.length > 0) {
+    if (result.rowCount > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
@@ -47,7 +47,7 @@ class UsersService {
 
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError('User tidak ditemukan');
     }
 
